517a241d4bf59b7243473c3ae1d74ab20cf88c7a
Võ Bảo Long <75128770+vobaolong@users.noreply.github.com>
2023-08-12 11:45:33 +0700

    create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPage } from "./routes/Routes.js";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
